994dc1eb403d658369627975e64cc28df3f7d564
Om Tambat <136994090+Onix284@users.noreply.github.com>
2025-06-06 10:57:45 +0530

    Update SIgnUpPage.kt

--- Client/Lawyer Spot/composeApp/src/commonMain/kotlin/org/example/project/SIgnUpPage.kt
+++ Client/Lawyer Spot/composeApp/src/commonMain/kotlin/org/example/project/SIgnUpPage.kt
@@ -1,5 +1,6 @@
 package org.example.project
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,22 +10,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,8 +34,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
@@ -55,14 +54,27 @@ fun SignUpScreen(){
 
         //Elements Columns
         Column(modifier = Modifier.padding(5.dp)
-            .padding(top = 30.dp)
+            .padding(top = 10.dp)
         ){
             //Header
-            Text("Sign Up", fontWeight = FontWeight.Bold, fontSize = 30.sp)
+            Row{
+                Box(modifier = Modifier.fillMaxWidth()){
+                    Text(text = "Sign Up", fontWeight = FontWeight.Bold, fontSize = 30.sp,
+                        modifier = Modifier.align(alignment = Alignment.CenterStart))
+
+                    AsyncImage(
+                        model = "https://randomuser.me/api/portraits/men/75.jpg",
+                        contentDescription = null,
+                        modifier = Modifier.align(alignment = Alignment.CenterEnd)
+                            .size(70.dp)
+                            .clip(RoundedCornerShape(90.dp)),
+                    )
+                }
+            }
 
             Spacer(modifier = Modifier.height(15.dp))
 
-            Text("Please signup before get started.", fontWeight = FontWeight.Light, fontSize = 17.sp)
+            Text(text = "Please signup before get started.", fontWeight = FontWeight.Light, fontSize = 17.sp)
 
             Spacer(modifier = Modifier.height(20.dp))
 
@@ -112,40 +124,77 @@ fun SignUpScreen(){
                 modifier = Modifier.fillMaxWidth()
             )
 
-            Spacer(modifier = Modifier.height(30.dp))
 
-            //Profile Picture
-            Box(modifier = Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.height(20.dp))
 
-                Column(modifier = Modifier.fillMaxWidth()){
+            //Select Role
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Select Role", modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
+                    .weight(0.4f))
 
-                    AsyncImage(
-                        model = "https://randomuser.me/api/portraits/men/75.jpg",
-                        contentDescription = null,
-                        modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
-                            .clip(RoundedCornerShape(90.dp))
-                            .size(150.dp)
+
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = {expanded = !expanded},
+                    modifier = Modifier.weight(0.6f)
+                ) {
+                    OutlinedTextField(
+                        value = selectedRole,
+                        onValueChange = {},
+                        readOnly = true,
+                        label = {Text("Select Role")},
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                            .menuAnchor()
                     )
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    ElevatedButton(onClick = {},
-                        modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = {expanded = false}
                     ){
-                        Text("Upload Profile Photo")
+                        roles.forEach { role ->
+                            DropdownMenuItem(
+                                onClick = {
+                                    selectedRole = role
+                                    expanded = false
+                                },
+                                text = { Text(text = role) }
+                            )
+                        }
                     }
                 }
             }
 
-            Spacer(modifier = Modifier.height(20.dp))
-
-            Row {
-                Text("Select Role")
+            Spacer(modifier = Modifier.height(25.dp))
 
 
+            OutlinedButton (onClick = {}, modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 50.dp)
+                .height(50.dp)){
+                Text("Update Profile Picture")
             }
 
-        }
 
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Box(modifier = Modifier.fillMaxSize()){
+                FilledTonalButton(onClick = {}, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 90.dp)
+                    .height(50.dp)){
+                    Text("Signup", fontSize = 15.sp)
+                }
+           }
+
+            Spacer(modifier = Modifier.height(20.dp))
+            
+
+                Text(text = "Already have an account? ")
+                Text(text = "Login Now ")
+
+        }
     }
 }
